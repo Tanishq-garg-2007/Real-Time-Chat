726b5b4d524420cd305b1ac5f28e0e0685d42bcf
Tanishq-garg-2007 <iit2024038@iiita.ac.in>
2025-06-03 12:13:54 +0530

    Add files via upload

---
+++ Socket_Learn/server/app.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import { Server } from 'socket.io'
+import {createServer} from 'http'
+import cors from 'cors'
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:5173",
+        methods:["GET","POST"],
+        credentials:"true"
+    },
+});
+
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+})
+
+io.on("connection",(socket)=>{
+    console.log("User connected");
+    console.log("Id",socket.id);
+
+    socket.on("message",({message,room,user_name})=>{
+        console.log(message);
+        io.to(room).emit("receive-message",{message,user_name});
+    });
+
+    socket.on("join-room",(room)=>{
+        socket.join(room);
+        console.log(`User Joined room ${room}`);
+    });
+
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected",socket.id)
+    });
+})
+
+server.listen(3000,()=>{
+    console.log("Server Is running on port 3000") 
+})
