65b3063fe773c36ba9a6e5d319ac0f9ff6948872
Tanishq-garg-2007 <iit2024038@iiita.ac.in>
2025-06-12 07:30:33 +0000

    Jun 12, 2025, 1:00 PM

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const mongoDb = async() =>{
     try {
-        await mongoose.connect("mongodb+srv://iit2024038:1234%40abcd@cluster0.rqfco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect("mongodb+srv://iit2024038:SdFBKj2nlMZDRO7W@cluster0.v0oewko.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         console.log("Mongo Db Connected Successfully");
     } catch (error) {
         console.log("Mongo DB connection failed");
