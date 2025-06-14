b8cf63e983237239e500c215bbaeec87935ec3b7
Tanishq-garg-2007 <iit2024038@iiita.ac.in>
2025-06-14 11:30:27 +0530

    Add files via upload

---
+++ Socket_Learn/server/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const mongoDb = async() =>{
+    try {
+        await mongoose.connect("mongodb+srv://iit2024038:SdFBKj2nlMZDRO7W@cluster0.v0oewko.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        console.log("Mongo Db Connected Successfully");
+    } catch (error) {
+        console.log("Mongo DB connection failed");
+    }
+}
+
+export default mongoDb;
